9bb47c21c97f3f6a47a9338fe8633dc6e409ab72
Junaed Muhammad Chowdhury <34476904+Junaed29@users.noreply.github.com>
2020-04-12 12:57:49 +0600

    GET methods in RETROFIT

--- app/src/main/java/com/example/retrofit/JsonPlaceHolderApi.java
+++ app/src/main/java/com/example/retrofit/JsonPlaceHolderApi.java
@@ -1,9 +1,14 @@
 package com.example.retrofit;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 /******  All type of json operation work in here  ******/
 public interface JsonPlaceHolderApi {
@@ -12,4 +17,43 @@ public interface JsonPlaceHolderApi {
      * this function will return list of a Post type object***/
     @GET("posts")
     Call<List<Post>> getPosts();
+
+    //  We can use direct URL to get list
+    @GET
+    Call<List<Post>>  getPosts(@Url String url);
+
+
+    //   '/posts?userId=1'
+    @GET("posts")
+    Call<List<Post>> getPosts(@Query("userId") int userId);
+
+
+    //  '/posts?userId=1&_sort=id&_order=desc'
+    //   'int' value can be null so we will user Integer
+    //   'Integer' can be null
+    @GET("posts")
+    Call<List<Post>> getPosts(@Query("userId") Integer userId,
+                              @Query("_sort") String sort,
+                              @Query("_order") String orderBy
+    );
+
+
+    //   if we want more than one userId post we can use array
+    @GET("posts")
+    Call<List<Post>> getPosts(@Query("userId") Integer[] userId,
+                              @Query("_sort") String sort,
+                              @Query("_order") String orderBy
+    );
+
+    //   if we want more one userId and other operation we can use map
+    @GET("posts")
+    Call<List<Post>> getPosts(@QueryMap Map<String, String> parameters);
+
+
+    /* this is Retrofit ready function
+     * this function will return list of a Post type object for a specific id***/
+
+    // @GET("posts/'2'/comments") '2' can be changed by us just add some value
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") int postId);
 }
--- app/src/main/java/com/example/retrofit/MainActivity.java
+++ app/src/main/java/com/example/retrofit/MainActivity.java
@@ -1,14 +1,14 @@
 package com.example.retrofit;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,7 +18,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    TextView textView, captionTextView;
 
     /*****Retrofit object******/
     Retrofit retrofit;
@@ -34,18 +34,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        // ProgressDialog
         progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("Loading");
         progressDialog.setMessage("Data is getting ready");
         progressDialog.show();
 
+
         textView = findViewById(R.id.textResultId);
+        captionTextView = findViewById(R.id.captionTextViewId);
 
         /** Build a retrofit object **/
         retrofit = new Retrofit.Builder().
-                /** Base url **/
+                /*  Base url
+                 *  '/' must be put at the last of the url**/
                         baseUrl("https://jsonplaceholder.typicode.com/").
                 /** Converter which convert JSON to java object **/
                         addConverterFactory(GsonConverterFactory.create()).
@@ -54,6 +56,193 @@ public class MainActivity extends AppCompatActivity {
         /* JsonPlaceHolderApi is a interface so we can't directly create object
          * with help of retrofit object we get jsonPlaceHolderApi object **/
         jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+
+        getPost("https://jsonplaceholder.typicode.com/posts");
+
+        //getComment(5);
+
+        // If we don't need any specification then just pass null
+        //getSpecificPost(null, null, null);
+
+        //getSpecificPost(5, "id", "desc");
+
+        //getSpecificPost();
+    }
+
+
+    // Specific Post
+    public void getSpecificPost(int postId) {
+
+        captionTextView.setText("\"GET    /posts?userId=" + postId + "\"" + "\n" + "Posts for '" + postId + "' userId");
+
+
+        Call<List<Post>> allPostCall = jsonPlaceHolderApi.getPosts(postId);
+
+        /* allPostCall.execute can freeze the app
+         * so we call allPostCall.enqueue which is retrofit default method to word background thread  ***/
+        allPostCall.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                if (!response.isSuccessful()) {
+                    /* sometime response will be ERROR: 404 or
+                     * 404 Not Found **/
+                    textView.setText("error : " + response.code());
+
+                    /* this return for null point exception***/
+                    return;
+                }
+
+                /* get all list of Post class object
+                 * by response body*/
+                List<Post> allPost = response.body();
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (Post post : allPost) {
+                    stringBuilder.append("User Id : " + post.getUserId() + "\n");
+                    stringBuilder.append("Id : " + post.getId() + "\n");
+                    stringBuilder.append("Title : " + post.getTitle() + "\n");
+                    stringBuilder.append("Message : " + post.getMessage() + "\n\n");
+
+                }
+
+
+                textView.setText(stringBuilder.toString());
+
+                // ProgressDialog Close
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                /* Throwable is the super call of exception and errors***/
+                textView.setText(t.getMessage());
+            }
+        });
+    }
+
+
+    // Specific Post By Map
+    public void getSpecificPost() {
+
+        Map <String, String> parameter = new HashMap<>();
+
+        // HashMap can take key just one time so we cant use multiple userId here
+        parameter.put("userId", "2");
+        parameter.put("_sort", "id");
+        parameter.put("_order", "desc");
+
+        captionTextView.setText("Posts for using map");
+
+
+        Call<List<Post>> allPostCall = jsonPlaceHolderApi.getPosts(parameter);
+
+        /* allPostCall.execute can freeze the app
+         * so we call allPostCall.enqueue which is retrofit default method to word background thread  ***/
+        allPostCall.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                if (!response.isSuccessful()) {
+                    /* sometime response will be ERROR: 404 or
+                     * 404 Not Found **/
+                    textView.setText("error : " + response.code());
+
+                    /* this return for null point exception***/
+                    return;
+                }
+
+                /* get all list of Post class object
+                 * by response body*/
+                List<Post> allPost = response.body();
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (Post post : allPost) {
+                    stringBuilder.append("User Id : " + post.getUserId() + "\n");
+                    stringBuilder.append("Id : " + post.getId() + "\n");
+                    stringBuilder.append("Title : " + post.getTitle() + "\n");
+                    stringBuilder.append("Message : " + post.getMessage() + "\n\n");
+
+                }
+
+
+                textView.setText(stringBuilder.toString());
+
+                // ProgressDialog Close
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                /* Throwable is the super call of exception and errors***/
+                textView.setText(t.getMessage());
+            }
+        });
+    }
+
+    // More Specific Post sort and orderBy
+    public void getSpecificPost(Integer postId, String sort, String orderBy) {
+
+        captionTextView.setText("\"GET    /posts?userId=" + postId + "&_sort="+sort+"&_order="+orderBy+"\"" + "\n" + "Posts for '" + postId + "' userId sort by '"+sort+"' and '"+orderBy+"' order");
+
+
+        // If we don't need any specification then just pass null
+        Call<List<Post>> allPostCall = jsonPlaceHolderApi.getPosts(postId, sort, orderBy);
+
+
+        //   if we want more than one userId post we can use array
+        //Call<List<Post>> allPostCall = jsonPlaceHolderApi.getPosts(new Integer[]{1,2}, null, null);
+
+
+        /* allPostCall.execute can freeze the app
+         * so we call allPostCall.enqueue which is retrofit default method to word background thread  ***/
+        allPostCall.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                if (!response.isSuccessful()) {
+                    /* sometime response will be ERROR: 404 or
+                     * 404 Not Found **/
+                    textView.setText("error : " + response.code());
+
+                    /* this return for null point exception***/
+                    return;
+                }
+
+                /* get all list of Post class object
+                 * by response body*/
+                List<Post> allPost = response.body();
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (Post post : allPost) {
+                    stringBuilder.append("User Id : " + post.getUserId() + "\n");
+                    stringBuilder.append("Id : " + post.getId() + "\n");
+                    stringBuilder.append("Title : " + post.getTitle() + "\n");
+                    stringBuilder.append("Message : " + post.getMessage() + "\n\n");
+
+                }
+
+
+                textView.setText(stringBuilder.toString());
+
+                // ProgressDialog Close
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                /* Throwable is the super call of exception and errors***/
+                textView.setText(t.getMessage());
+            }
+        });
+    }
+
+    // All Post
+    public void getPost() {
+
+        captionTextView.setText("\"GET-   /posts\"" + "\n" + "All posts");
+
 
         Call<List<Post>> allPostCall = jsonPlaceHolderApi.getPosts();
 
@@ -77,16 +266,19 @@ public class MainActivity extends AppCompatActivity {
                 List<Post> allPost = response.body();
 
                 StringBuilder stringBuilder = new StringBuilder();
-                for(Post post : allPost){
-                    stringBuilder.append("User Id : "+post.getUserId()+"\n");
-                    stringBuilder.append("Id : "+post.getId()+"\n");
-                    stringBuilder.append("Title : "+post.getTitle()+"\n");
-                    stringBuilder.append("Message : "+post.getMessage()+"\n\n");
+                for (Post post : allPost) {
+                    stringBuilder.append("User Id : " + post.getUserId() + "\n");
+                    stringBuilder.append("Id : " + post.getId() + "\n");
+                    stringBuilder.append("Title : " + post.getTitle() + "\n");
+                    stringBuilder.append("Message : " + post.getMessage() + "\n\n");
 
                 }
 
-                progressDialog.dismiss();
+
                 textView.setText(stringBuilder.toString());
+
+                // ProgressDialog Close
+                progressDialog.dismiss();
             }
 
             @Override
@@ -95,6 +287,106 @@ public class MainActivity extends AppCompatActivity {
                 textView.setText(t.getMessage());
             }
         });
+    }
 
+    // All Post from directly from URL
+    public void getPost(String url) {
+
+        captionTextView.setText("\"Post By url\"" + "\n" + "All posts");
+
+
+        Call<List<Post>> allPostCall = jsonPlaceHolderApi.getPosts(url);
+
+        /* allPostCall.execute can freeze the app
+         * so we call allPostCall.enqueue which is retrofit default method to word background thread  ***/
+        allPostCall.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                if (!response.isSuccessful()) {
+                    /* sometime response will be ERROR: 404 or
+                     * 404 Not Found **/
+                    textView.setText("error : " + response.code());
+
+                    /* this return for null point exception***/
+                    return;
+                }
+
+                /* get all list of Post class object
+                 * by response body*/
+                List<Post> allPost = response.body();
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (Post post : allPost) {
+                    stringBuilder.append("User Id : " + post.getUserId() + "\n");
+                    stringBuilder.append("Id : " + post.getId() + "\n");
+                    stringBuilder.append("Title : " + post.getTitle() + "\n");
+                    stringBuilder.append("Message : " + post.getMessage() + "\n\n");
+
+                }
+
+
+                textView.setText(stringBuilder.toString());
+
+                // ProgressDialog Close
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                /* Throwable is the super call of exception and errors***/
+                textView.setText(t.getMessage());
+            }
+        });
+    }
+
+    // Specific Comments
+    public void getComment(int postId) {
+
+        captionTextView.setText("\"GET    /posts/" + postId + "/comments\"" + "\nComments for '" + postId + "' postId");
+
+        Call<List<Comment>> getAllCommentsCall = jsonPlaceHolderApi.getComments(postId);
+
+        /* allPostCall.execute can freeze the app
+         * so we call allPostCall.enqueue which is retrofit default method to word background thread  ***/
+        getAllCommentsCall.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if (!response.isSuccessful()) {
+                    /* sometime response will be ERROR: 404 or
+                     * 404 Not Found **/
+                    textView.setText("error : " + response.code());
+
+                    /* this return for null point exception***/
+                    return;
+                }
+
+                /* get all list of Comment class object
+                 * by response body*/
+                List<Comment> allComment = response.body();
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (Comment comment : allComment) {
+                    stringBuilder.append("Post Id : " + comment.getPostId() + "\n");
+                    stringBuilder.append("Id : " + comment.getId() + "\n");
+                    stringBuilder.append("Name : " + comment.getName() + "\n");
+                    stringBuilder.append("Email : " + comment.getEmail() + "\n");
+                    stringBuilder.append("Message : " + comment.getMessage() + "\n\n");
+
+                }
+
+
+                textView.setText(stringBuilder.toString());
+
+                // ProgressDialog Close
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                /* Throwable is the super call of exception and errors***/
+                textView.setText(t.getMessage());
+            }
+        });
     }
 }
