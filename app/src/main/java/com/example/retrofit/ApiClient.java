396b2e6296c8afed208859216212e28c03e280a2
Junaed Muhammad Chowdhury <34476904+Junaed29@users.noreply.github.com>
2020-04-17 19:13:36 +0600

    Api Client Added

---
+++ app/src/main/java/com/example/retrofit/ApiClient.java
@@ -0,0 +1,31 @@
+package com.example.retrofit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+//This is better for use in main Activity
+
+public class ApiClient {
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(loggingInterceptor).build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
+                    .build();
+        }
+
+        return retrofit;
+    }
+}
